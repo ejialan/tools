7490eb6327a200cbb6ffc551f0467260c2bb6250
ejialan <lanjiangang@gmail.com>
2013-11-20 00:34:31 +0800

    add sysgit

---
+++ sysgit/sysgit.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+precheck()
+{
+    :
+}
+
+generategitignore()
+{
+    :
+}
+
+init_repo()
+{
+    :
+}
+
+backup_empty_folder()
+{
+    :
+}
+
+backup_ownership()
+{
+    :
+}
+
+commit_repo()
+{
+    :
+}
+
