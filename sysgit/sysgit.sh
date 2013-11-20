f09ad25bf14b7b1ad28132afa4eae8346d071a1a
ejialan <jiangang.lan@ericsson.com>
2013-11-20 17:53:19 +0800

    [sysgit] add some implement hints

--- sysgit/sysgit.sh
+++ sysgit/sysgit.sh
@@ -17,11 +17,14 @@ init_repo()
 
 backup_empty_folder()
 {
+    find / -type d -empty  2>/dev/null | egrep -v '/proc' | xargs -I {} stat -c '%a %U:%G %n' {} > /emptyfolder.list
     :
 }
 
 backup_ownership()
 {
+    #stat -c '%a %U:%G %n'
+    git status / | grep 'new file' | awk '{print $4}' | xargs -I {} stat -c '%a %U:%G %n' {} > /stat.list
     :
 }
 
