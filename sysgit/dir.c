375fd58f92231321fbd3b0e1cb45206825de01d4
ejialan <lanjiangang@gmail.com>
2013-12-09 23:35:19 +0800

    refactor code

--- sysgit/dir.c
+++ sysgit/dir.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <string.h>
 
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
 struct ign_file
 {
   char* buf;
@@ -13,18 +15,60 @@ struct ign_file
   int is_dir;
 };
 
-int path_root_len = 0;
+struct path_buf
+{
+  char* buf;
+  int prefix_len;
+  int len;
+  int cap;
+};
+
 struct ign_file* ign_files;
 int nr_ign_files = 0;
 
-int 
-append_str(char *dst, char *src)
+void
+reset_path(struct path_buf *path, int len)
+{
+  path->len = len;
+  path->buf[len] = '\0';
+}
+
+void 
+ensure_cap(struct path_buf *path, int extra)
+{
+  if(path->len + extra > path->cap)
+  {
+    int add = MAX(2*extra, 1024);
+    path->buf = realloc(path->buf, path->cap + add);
+    path->cap += add;
+  }
+}
+
+void 
+print_path(struct path_buf *path)
+{
+  if(path->len>0)
+    printf("len = %d, cap = %d, str = %s\n", path->len, path->cap, path->buf);
+}
+
+void 
+_append_str(struct path_buf *path, const char *src)
 {
     int len = strlen(src);
-    memcpy(dst, src, len);
-    dst[len] = '\0';
-    return len;
+    ensure_cap(path, len);
+
+    memcpy(path->buf + path->len, src, len);
+    path->len += len;
+    path->buf[path->len] = '\0';
 }
+
+void
+append_path_seperator(struct path_buf *path)
+{
+  if(path->buf[path->len-1] != '/')
+    _append_str(path, "/");
+}
+
 
 int is_dot_or_dotdot(const char* name)
 {
@@ -111,16 +155,18 @@ void load_ignore_list(const char* path, int linenr)
 }
 
 int
-browse_dir(char* path, int len)
+browse_dir(struct path_buf *path)
 {
     struct dirent **ep;
     int entries;
     int i;
+    int current_path_len;
 
-    if(path[len-1] != '/')
-        len += append_str(path+len, "/");
+    append_path_seperator(path);
 
-    entries = scandir(path, &ep, 0, alphasort);
+    current_path_len = path->len;
+
+    entries = scandir(path->buf, &ep, 0, alphasort);
     if(entries < 0)
     {
         perror("Failed to scan directory");
@@ -130,20 +176,20 @@ browse_dir(char* path, int len)
     for(i=0; i<entries; i++)
     {
         //puts(ep[i]->d_name);
+        reset_path(path, current_path_len);
         if(!is_dot_or_dotdot(ep[i]->d_name) && !is_dot_git(ep[i]->d_name))
         {
             struct stat sb;
-            int child_len = len;
-            child_len += append_str(path+child_len, ep[i]->d_name);
+            _append_str(path, ep[i]->d_name);
 
-            if (lstat(path, &sb) == 0)
+            if (lstat(path->buf, &sb) == 0)
             {
               if(file_type(sb.st_mode) == 'd')
               {
-                child_len += append_str(path+child_len, "/");
+                append_path_seperator(path);
               }
 
-              if(is_ignored(path + path_root_len))
+              if(is_ignored(path->buf + path->prefix_len))
                 continue;
 
               printf ("%c %d %d%d%d %d:%d %s\n", 
@@ -154,9 +200,9 @@ browse_dir(char* path, int len)
                          /* human readable file mode */
                          (sb.st_mode & S_IRWXU) >> 6, (sb.st_mode & S_IRWXG) >> 3, sb.st_mode & S_IRWXO, 
                          /* uid and gid */
-                         sb.st_uid, sb.st_gid, path + path_root_len);
+                         sb.st_uid, sb.st_gid, path->buf + path->prefix_len);
               if(S_ISDIR(sb.st_mode))
-                 browse_dir(path, child_len);
+                 browse_dir(path);
             }
             else
             {
@@ -169,15 +215,46 @@ browse_dir(char* path, int len)
     return 0;
 }
 
+int 
+tranversal_dir ( const char* path, int argc, const char **argv)
+{
+  return 0;
+}
+
+int 
+restore_dir ( const char* path, int argc, const char **argv)
+{
+  return 0;
+}
+
+int 
+clean_dir ( const char* path, int argc, const char **argv)
+{
+  return 0;
+}
+
+struct cmd_struct
+{
+  const char* cmd;
+  int (*fn)(const char*, int, const char**);
+};
+
+struct cmd_struct commands[] = {
+  {"tranversal", tranversal_dir},
+  {"restore", restore_dir},
+  {"clean", clean_dir}
+};
 
 int
 main (int argc, char**argv)
 {
-    char path[10240];
-    int len = append_str(path, argv[1]);
-    if(path[len-1] != '/')
-        len += append_str(path+len, "/");
-    path_root_len = len;
-    load_ignore_list(argv[2], atoi(argv[3]));
-    return browse_dir(path, len);
+  struct path_buf path = {
+    NULL, 0, 0, 0
+  };
+  _append_str(&path, argv[1]);
+  append_path_seperator(&path);
+  path.prefix_len = path.len;
+
+  load_ignore_list(argv[2], atoi(argv[3]));
+  return browse_dir(&path);
 }
