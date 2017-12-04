fac2bdfc59eae0769525885be420d8f8362524c3
lanjiangang <jiangang.lan@ele.me>
2017-12-04 12:38:50 +0800

    change default interface to any

--- pcapsim/monitor.py
+++ pcapsim/monitor.py
@@ -7,4 +7,4 @@ def monitor_callback(pkt):
      print """receive ____________________________________"""
      pkt.show()
 
-sniff(prn=lambda x : monitor_callback(x), filter="port 3080 and host 127.0.0.1", iface="lo", store=0)
+sniff(prn=lambda x : monitor_callback(x), filter="port 3080 and host 127.0.0.1", iface="any", store=0)
